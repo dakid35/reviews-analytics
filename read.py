7adba9b52f6458650d32d742ecfd5bb83b41a5d6
Kent Chen <dakid0216@gmail.com>
2021-11-23 15:54:19 +0800

    clac avg of reviews

--- read.py
+++ read.py
@@ -17,8 +17,13 @@ with open('reviews.txt', 'r') as f:
 		count += 1 #count = count + 1
 		if count % 1000 == 0:  # %餘數7%3=1 , 10%6=4
 		    print(len(data))
-print(len(data))
+print('檔案讀取完了, 總共有', len(data), '筆資料')
 
-print(data[0])
-print('---------------')
-print(data[1])
+#test 算出每筆資料的平均長度
+
+sum_len = 0
+for d in data:
+	sum_len = sum_len + len(d) #sum_len += 1
+
+print('留言的平均長度為', sum_len/len(data))
+
